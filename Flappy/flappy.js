e971e526fa9f4e752bee322121800291556b08c5
Tushar <tushar.rishav@gmail.com>
2014-09-09 20:26:22 +0530

    update!

--- Flappy/flappy.js
+++ Flappy/flappy.js
@@ -1,55 +1,125 @@
-    var canvasGame = document.getElementById('game_canvas');
-    var ctxGame = canvasGame.getContext('2d');
+   (function(){
+     canvasGame = document.getElementById('game_canvas');
+     ctxGame = canvasGame.getContext('2d');
      
-    var canvasPipes = document.getElementById('pipes_canvas');
-    var ctxPipes = canvasPipes.getContext('2d');
+     canvasPipes = document.getElementById('pipes_canvas');
+     ctxPipes = canvasPipes.getContext('2d');
      
-    var canvasFlappy = document.getElementById('flappy_canvas');
-    var ctxFlappy = canvasFlappy.getContext('2d');
+     canvasFlappy = document.getElementById('flappy_canvas');
+     ctxFlappy = canvasFlappy.getContext('2d');
 
    
     ctxGame.fillStyle = "hsla(0, 0%, 0%, 0.5)";
     ctxGame.font = "bold 20px Arial";
 
-     
-    var obstacle=[], gameWidth=canvasPipes.width, gameHeight=canvasPipes.height,bgDrawX1 = 0,bgDrawX2 = 2400;
-    var  isPlaying=true;
-     
-    var requestAnimFrame =  window.requestAnimationFrame ||            //adding browser compatibility
-                            window.webkitRequestAnimationFrame ||
-                            window.mozRequestAnimationFrame ||
-                            window.oRequestAnimationFrame ||
-                            window.msRequestAnimationFrame ||
-                            function(callback) {
-                                window.setTimeout(callback, 1000 / 60);        //in worse case of some failure or some exceptions
-                            };
-     
-     
-    background = new Image();
-    background.src = 'images/flappy.png';
-
-    bigS=new Image();
+     bigS=new Image();
     bigS.src='images/birdie.png';
+   
 
-    bird=new Image();
-    bird.src='images/bird.png';
+    birdChoosen=false;
+     obstacle=[], gameWidth=canvasPipes.width, gameHeight=canvasPipes.height,bgDrawX1 = 0,bgDrawX2 = 2400,colorCode=[],pipe_speed=3,bg_speed=5;
+      isPlaying=true;
 
+    })();
 
-     
-    flappy=new createFlappy();                                 //creating the flappy bird object
-     
-     
-     
-     
-    background.addEventListener('load', start, false);
-     
-    function start()
-    {     
-         flappy.playerName=prompt("Your name buddy ","Mr x");
-         alert("use spacebar to control the birdie!");
-         definePipes();
-         welcomeMsg();
+    window.requestAnimFrame = function(){
+    return (
+        window.requestAnimationFrame       ||        //adding browser compatibility
+        window.webkitRequestAnimationFrame || 
+        window.mozRequestAnimationFrame    || 
+        window.oRequestAnimationFrame      || 
+        window.msRequestAnimationFrame     || 
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        }
+    );
+}();
+    function birdType(choice){
+
+      switch(choice){
+        case 1:{
+           bird=new Image();
+           bird.src="images/phoenix.png";                                                   
+          flappy=new createFlappy(10,190,95,90,1);
+          
+          break;
+        }
+       
+        case 2:{
+           bird=new Image();
+          bird.src="images/bird.png";
+          flappy=new createFlappy(0,70,63,50,2);      //blue berry
+
+          break;
+        }
         
+         case 3:{
+          
+          bird=bigS;
+          flappy=new createFlappy(3,490,17,12.5,3);        //normal flappy
+          break;
+        }
+
+        default:alert("no bird chosen");
+      }
+      birdChoosen=true;
+
+      start();
+     
+
+    }
+     createFlappy.prototype.animateFlappy=function(){
+
+       switch(this.indx)
+       {
+        case 1:
+        {
+          this.radius=20;
+          if(this.srcX<375&&this.srcX>9)
+                this.srcX+=90;
+
+            else
+              this.srcX=10;
+          break;
+        }
+
+        case 2:
+           {
+              this.radius=17;
+            if(this.srcX<255&&this.srcX>=0)
+                this.srcX+=64;
+            if(this.srcX>255)
+              this.srcX=0;
+            
+            break;
+           }
+       
+        case 3:
+              { if(this.srcX<76&&this.srcX>=3)
+                  this.srcX+=28;
+             
+                if(this.srcX>=76)
+                  this.srcX=3;
+               // console.log(this.srcX);
+                break;
+              }
+       
+       }
+    
+  }
+
+    
+    function start()
+    {
+          if(birdChoosen)
+        {
+         
+          document.getElementById("top").style.display="none";
+          flappy.playerName=prompt("Your name buddy ","Mr x");
+          alert("use spacebar to control the birdie!");
+          definePipes();
+          welcomeMsg();
+        }
     }
 
     function welcomeMsg(){
@@ -65,6 +135,7 @@
             ctxPipes.clearRect(0,0,gameWidth,gameHeight);
                     
             ctxGame.drawImage(bigS,394,57,97,25,gameWidth/3,gameHeight/3,gameWidth/4,gameHeight/4);
+            ctxGame.fillText(flappy.playerName+" Score: " + score, gameWidth/3,gameHeight-50);  // for adding score at the end
           }
            
 
@@ -110,8 +181,12 @@
     function drawBg() {
      
         ctxGame.clearRect(0, 0, gameWidth, gameHeight);
-        ctxGame.drawImage(background, 0, 465, 2400, gameHeight, bgDrawX1, 466, 2400, gameHeight);  //backg 1
-        ctxGame.drawImage(background, 0, 465, 2400, gameHeight, bgDrawX2, 466, 2400, gameHeight);   //backg 2*/
+        //ctxGame.drawImage(background, 0, 465, 2400, gameHeight, bgDrawX1, 466, 2400, gameHeight);  //backg 1
+        //ctxGame.drawImage(background, 0, 465, 2400, gameHeight, bgDrawX2, 466, 2400, gameHeight);   //backg 2*/
+        
+        ctxGame.drawImage(bigS, 292.5, 0, 107.5, 55, bgDrawX1, 466, 2400, 250);  //backg 1
+        ctxGame.drawImage(bigS, 292.5, 0, 107.5, 55, bgDrawX2, 466, 2400, 250);   //backg 2*/
+        
         ctxGame.fillText(flappy.playerName+" Score: " + score, gameWidth/3,gameHeight-50);
 
     }
@@ -148,7 +223,7 @@
      
             for(i=0;i<250;i++)
             {  
-              obstacle[i].x_init-=3;        //all pipes shifted to left by 3px;
+              obstacle[i].x_init-=pipe_speed;        //all pipes shifted to left by 3px;
             }
      
             movePipes();
@@ -166,16 +241,23 @@
      
      
     function moveBg() {
-        bgDrawX1 -= 5;
-        bgDrawX2 -= 5;
+        bgDrawX1 -= bg_speed;
+        bgDrawX2 -= bg_speed;
                                     //changing coordinate data of obstacle
                
-        if (bgDrawX1 <= -2400) {           //if backg 1 moves to extreme left shift it to extreme right
+        if (bgDrawX1 <=-2400) {           //if backg 1 moves to extreme left shift it to extreme right
             bgDrawX1 = 2400;
-        } else if (bgDrawX2 <= -2400) {
+        } else if (bgDrawX2 < -2400) {
             bgDrawX2 = 2400;
         }
         drawBg();
+        bg_speed+=0.02;
+        pipe_speed+=0.001
+
+        if(pipe_speed>4)
+          pipe_speed=3;
+        if(bg_speed>25)
+          bg_speed=5;
     }
      
      
@@ -184,7 +266,7 @@
      
     var obstacle=[];
     var c=0;
-    function createObstacle(X,H){
+    function createObstacle(X,H,clr){
      this.index=c++;
      this.x_init=X;
      this.height=H;           //height of top pipe
@@ -192,6 +274,7 @@
      this.hole=100;          //hole in between them
      this.gap=250;           //gap between two obstacles
      this.totalHeight=465; 
+     this.fStyle=clr;
      //console.log(arguments);      //height of toppipe+bottompipe+hole
     }
      
@@ -200,8 +283,6 @@
       function movePipes(){
          
           wipePipes();
-       
-     
           drawPipes();
      
       }
@@ -214,28 +295,33 @@
                       h1=Math.floor(Math.random()*365);           // 465 is total height of Pipe area
                       h2=Math.floor(Math.random()*365);                
                         
-                      while( !((Math.abs(h1-h2)<250)&&(h1<365)&&(h2<365)) )  // for making it playable
+                      while( !((Math.abs(h1-h2)<250)&&(h1<365)&&(h2<365)&&h1>40) )  // for making it playable
                       {
                          h1=Math.floor(Math.random()*365);
                          h2=Math.floor(Math.random()*365);
 
                       }
-                          //if(!i)
-                          //console.log(h1+" "+h2);          
+
+                        for(var c=0;c<3;c++)
+                        colorCode[c]=Math.floor(Math.random()*255);        
                            
                          x1+=200;
-                         obstacle[i]= new createObstacle(x1,h1);
+                         obstacle[i]= new createObstacle(x1,h1,colorCode);
                          x1+=200;  
-                         obstacle[i+1]= new createObstacle(x1,h2);
+                         obstacle[i+1]= new createObstacle(x1,h2,colorCode);
      
                         if(i==249)
                                break;
                         
            }
+
+            
+           
      
     }
      
     function drawPipes(){
+           
      
       for(i=0;i<250;i+=2)
       {   
@@ -243,11 +329,11 @@
           
           if(pattern<10)
           {
-            ctxPipes.fillStyle="skyblue";
+            ctxPipes.fillStyle= "rgb("+obstacle[i].fStyle[0]+","+obstacle[i].fStyle[1]+","+obstacle[i].fStyle[2]+")";
             ctxPipes.fillRect(obstacle[i].x_init,0,obstacle[i].width,obstacle[i].height);
             ctxPipes.fill();
             
-            ctxPipes.fillStyle="blue";                              
+            ctxPipes.fillStyle= "rgb("+obstacle[i].fStyle[0]+","+obstacle[i].fStyle[2]+","+obstacle[i].fStyle[1]+")";                              
             ctxPipes.fillRect(obstacle[i].x_init,(100+obstacle[i].height),obstacle[i].width,367-obstacle[i].height);
             ctxPipes.fill();
 
@@ -256,11 +342,11 @@
          else
          {
 
-            ctxPipes.fillStyle="lightgreen";
+            ctxPipes.fillStyle="rgb("+obstacle[i].fStyle[2]+","+obstacle[i].fStyle[1]+","+obstacle[i].fStyle[0]+")";
             ctxPipes.fillRect(obstacle[i].x_init,0,obstacle[i].width,obstacle[i].height);
             ctxPipes.fill();
             
-            ctxPipes.fillStyle="green";                              
+            ctxPipes.fillStyle="rgb("+obstacle[i].fStyle[1]+","+obstacle[i].fStyle[0]+","+obstacle[i].fStyle[2]+")";                              
             ctxPipes.fillRect(obstacle[i].x_init,(100+obstacle[i].height),obstacle[i].width,367-obstacle[i].height);
             ctxPipes.fill();
 
@@ -271,23 +357,20 @@
       
      
      }
+    
      function wipePipes(){
      
       ctxPipes.clearRect(0,0,gameWidth,gameHeight);
      }
      
-     function createFlappy(){
+     function createFlappy(sX,sY,sW,sH,index){
      
-     /* this.srcX=30.5;
-      this.srcY=490;
-      this.width=18.5;
-      this.height=12.5;*/
 
-      this.srcX=0;
-      this.srcY=70;
-      this.width=62.5;
-      this.height=50;
-
+      this.srcX=sX;
+      this.srcY= sY;
+      this.width=sW;
+      this.height=sH;
+      this.indx=index;
       this.drawX=50;
       this.drawY=100;
        
@@ -297,32 +380,17 @@
       this.isSpacebar=false;
      
     }
+ var fCounter=0;
+   
 
-    createFlappy.prototype.animateFlappy=function(){
-
-     /* if(this.srcX<59&&this.srcX>2){
-        this.srcX+=28.5;
-      }
-      else
-        this.srcX=2.5;*/
-     
-       if(this.srcX<254&&this.srcX>-1){
-        this.srcX+=65;
-      }
-      else
-        this.srcX=0;
-
-       fAnimCntrl=setInterval(this.animateFlappy,20000);
-    }
-     
     createFlappy.prototype.draw=function(){
       if(!(this.isSpacebar))
       {
         this.drawY+=4;
         if( (this.drawY+this.radius)>=415)         //flappy falls down
-          {  isPlaying=false;
-             flappy.playerName+=" Game Over";
-                    welcomeMsg();
+          {  
+            isPlaying=false;
+            welcomeMsg();
      
           }
 
@@ -332,9 +400,9 @@
       else
       {  
         if(this.drawY<0)
-          this.drawY= 0;              //for upper limit of motion of flappy
+          this.drawY=0;              //for upper limit of motion of flappy
         else  
-        this.drawY-=4;
+        this.drawY-=3.5;
       }
      
      
@@ -363,7 +431,7 @@
                     moveBg();
                     drawBg();  //adding game over message before exiting
                     isPlaying=false;
-                    window.clearInterval(fAnimCntrl);
+                    
                     window.cancelAnimationFrame(animationControl);
                     welcomeMsg();
 
